490e0a252194c2ee55efc6073ed8b14dc8c9426b
Brandon Tilley <brandon@brandontilley.com>
2011-11-28 17:53:35 -0800

    Blizzard forum signature (take 1)

---
+++ us.battle.net.js
@@ -0,0 +1,10 @@
+$(function() {
+  var signature   = "\n________________________________________________\n" +
+    "MVP of Something or Other\n" +
+    "Libraries for the Community Platform API: http://us.battle.net/wow/en/forum/topic/2369882588";
+  var textarea    = $("textarea.post-editor");
+  var currentText = textarea.val();
+  if (currentText.indexOf(signature) == -1) {
+    textarea.val(currentText + signature);
+  }
+});

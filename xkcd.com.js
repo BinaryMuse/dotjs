afdf126c6d0bc81d701340e97fbe3856b92857e8
Brandon Tilley <brandon@brandontilley.com>
2012-07-11 13:22:41 -0700

    Update xkcd script

--- xkcd.com.js
+++ xkcd.com.js
@@ -1,9 +1,10 @@
 // xkcd title text under the image
-var image     = $("div#contentContainer img[title]");
+var image     = $("#comic img[title]");
+console.log(image);
 var paragraph = $("<p>" + image.attr('title') + "</p>");
 image.attr('title', '');
 paragraph.css({"font-variant": "normal",
                "border": "1px solid yellow",
                "padding": "1em",
                "background-color": "#ffc"});
-image.after(paragraph);
+image.parents('#comic').after(paragraph);

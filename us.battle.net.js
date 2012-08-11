fd6495ddbe51cddfa38f7ff842ff79dc7d565e29
Brandon Tilley <brandon@brandontilley.com>
2012-08-10 17:40:35 -0700

    Add link to Battle.net signature

--- us.battle.net.js
+++ us.battle.net.js
@@ -1,7 +1,8 @@
 $(function() {
   var signature   = "\n________________________________________________\n" +
     "MVP, Web and API\n" +
-    "Libraries for the Community Platform API: http://us.battle.net/wow/en/forum/topic/2369882588";
+    "Libraries for the Community Platform API: http://us.battle.net/wow/en/forum/topic/2369882588\n" +
+    "Libraries for the Community Platform API: http://us.battle.net/d3/en/forum/topic/6307731317";
   var textarea    = $("textarea.post-editor");
   var currentText = textarea.val();
   if (currentText.indexOf(signature) == -1) {
